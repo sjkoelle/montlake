c34ab1ee63801130281121796078c40dfe0bb5d7
Samson Johnathan Koelle <sjkoelle@stat.washington.edu>
2021-11-26 15:48:09 -0800

    a new shell script

--- experiments/configs/config_short.sh
+++ experiments/configs/config_short.sh
@@ -4,11 +4,13 @@
 #SBATCH --mem=10000
 #SBATCH --job-name=montlake
 #SBATCH --error=montlake.err
-#SBATCH --output=mda_montlake.log
+#SBATCH --output=montlake.log
 #SBATCH --mail-type=FAIL       # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=sjkoelle@gmail.com # Email to which notifications will be sent
 
+export PATH="~/anaconda3/bin:$PATH"
 source activate montlake
+cd ~/montlake
 python -u -m montlake.exec.run_exp --config /homes/sjkoelle/montlake/experiments/configs/rigidethanol_diagram.json --outdir /homes/sjkoelle/thesis_data/processed_data_2/rigidethanol --raw_data /homes/sjkoelle/thesis_data/raw_data/rigidethanol_nonoise.npy --nreps 25 --mflasso --name re_nonoise_diagram_mf
 source deactivate
 echo "end"
